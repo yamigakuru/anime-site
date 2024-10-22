52fa6da89f278c1183cce90880e05baae8aa2b88
yamigakuru <qforceplay@gmail.com>
2024-10-22 12:45:46 +0200

    homework 2

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,14 @@
-const modal = document.querySelector('.search-model');
-const modalBtn = document.querySelector('.icon_search');
-const modalClose = modal.querySelector('.search-close-switch');
-const search = modal.querySelector('#search-input');
+const modal = () => {
+    const modal = document.querySelector('.search-model');
+    const modalBtn = document.querySelector('.icon_search');
+    const modalClose = modal.querySelector('.search-close-switch');
+    const search = modal.querySelector('#search-input');
 
-modalBtn.addEventListener('click', () => modal.style.display = 'block' );
+    modalBtn.addEventListener('click', () => modal.style.display = 'block' );
 
-modalClose.addEventListener('click', () => modal.style.display = 'none' );
+    modalClose.addEventListener('click', () => modal.style.display = 'none' );
 
-search.addEventListener('input', () => console.log(search.value));
+    search.addEventListener('input', () => console.log(search.value));
+}
+
+modal()
